bcf063213c11d68cc373d5e90d4e2dfc3e0c4427
Uberi <azhang9@gmail.com>
2013-12-24 16:51:35 -0500

    More stuff on the server side.

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
 #!/usr/bin/python
 
 # TODO:
-# * set up a simple webserver to communicate with JS
 # * save the contents of the search box and serialized entries
 
+# standard library modules
 import os, sys
-from PySide import QtCore, QtGui
-from PySide.QtDeclarative import QDeclarativeView
-from PySide.QtWebKit import *
+import http.server
+import threading
 
+# external modules
+from PySide import QtCore, QtGui, QtWebKit
+from PySide.QtDeclarative import QDeclarativeView
+
+SERVER_ADDRESS = ("localhost", 222)
 SETTINGS_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "settings.conf")
 
 class PicruxWindow:
     def __init__(self):
         self.load_fonts()
-        
+
         # set up window
-        self.view = QWebView()
+        self.view = QtWebKit.QWebView()
         self.view.load(QtCore.QUrl("views/main.html"))
         self.view.setWindowTitle("Picrux v0.2")
         self.view.setWindowIcon(QtGui.QIcon("icon.png"))
         self.view.show()
-        
+
         # center window on screen
         position = self.view.frameGeometry()
         center = QtGui.QDesktopWidget().availableGeometry().center()
         position.moveCenter(center)
         self.view.move(position.topLeft())
 
-    def load():
+    def load(self):
         """Returns the contents of the settings file as a string"""
         with open(SETTINGS_FILE, "r") as f:
             return f.read()
 
-    def save(data):
+    def save(self, data):
         """Saves the string `data` to the settings file"""
         with open(SETTINGS_FILE, "w+") as f:
             f.write(data)
@@ -45,7 +49,32 @@ class PicruxWindow:
             if os.path.isfile(path):
                 QtGui.QFontDatabase.addApplicationFont(path)
 
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        pass
+
+    def do_GET(self):
+        if self.path == "/parse_time":
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(bytes("{\"x\":1}", "utf-8"))
+        else:
+            self.send_error(404, "Page \"%s\" not found" % self.path)
+
+    def do_POST(self):
+        pass
+
+def run_server():
+    httpd = http.server.HTTPServer(SERVER_ADDRESS, RequestHandler)
+    httpd.serve_forever()
+
 if __name__ == "__main__":
+    # start server
+    server = threading.Thread(target=run_server, daemon=True)
+    server.start()
+
+    # start application
     application = QtGui.QApplication(sys.argv)
     window = PicruxWindow()
     sys.exit(application.exec_())
--- views/js/picrux.js
+++ views/js/picrux.js
@@ -1,4 +1,7 @@
+//wip: move the time and settings stuff to the right side in its own pane
+
 var main = function() {
+	// editor setup
 	var editor = CodeMirror.fromTextArea($("#search_add > textarea").get(0), {
 		placeholder: "search or add entries...",
 		mode: "gfm",
@@ -6,46 +9,112 @@ var main = function() {
 		autofocus: true,
 		lineWrapping: true,
 	});
-	updateTimes();
-	setInterval(updateTimes, 1000);
+	editor.on("change", onAddSearchChange);
+	
+	// update times every second
+	setInterval(function() {
+		$(".entry").each(function(index, entry) {
+			Entry.updateTime($(entry));
+		})
+	}, 1000);
 	
 	// fade in the entries
 	$(".entry").hide().fadeIn();
 }
 
-var Entry = {
-	create: function(time, message) {
-		var element = $("#items") //wip: add an entry;
-		//wip: figure out how to make this work with "new"
-		return {
-			time: time,
-			message: message,
-			element: element,
-			__proto__: Entry,
-		};
-	},
-	destroy: function() {
-		//wip: remove the element
-	},
-};
-
-var updateTimes = function() {
-	$(".time").each(function(i, e) {
-		var element = $(e);
-		var time = moment.unix(element.data("time"));
-		element.text(time.fromNow());
-		if (time.isBefore())
-			element.parent().addClass("past");
-	})
+var updateTemporaryEntry = function(userMessage) {
+	var entry = $(".entry_editing");
+	
+	//wip: debug
+	$.get("http://localhost:222/parse_time", userMessage)
+		.done(function(data) {
+			alert(data);
+		})
+		.fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	
+	// parse time values
+	return //wip: need to make a request to the server to use recurrent to parse it
+	var times = chrono.parse(userMessage);
+	if (times.length > 0) {
+		entry.find(".userTime").val(times[0].text);
+		var timeDue = moment(times[0].startDate).unix();
+		Entry.time(entry, timeDue);
+	}
+	else {
+		entry.find(".userTime").val("")
+		Entry.time(entry, null);
+	}
+	
+	// convert the markdown to HTML
+	var valueHTML = marked(userMessage, {
+		gfm: true, tables: true, breaks: true, sanitize: false,
+		smartLists: true, smartypants: true,
+	});
+	entry.find(".message").html(valueHTML);
+	//wip: the temporary entry should allow editing of the time and message separately
 }
 
+// calls `updateTemporaryEntry` when the editor has not changed for a while
 var onAddSearchChange = function() {
 	var currentTimer;
 	return function(cm, change) {
 		clearTimeout(currentTimer);
-		currentTimer = setTimeout(function() {
-			cm.getValue();
-			//wip: render the HTML and update the element
-		}, 1000);
+		currentTimer = setTimeout(function () {
+			var value = cm.getValue();
+			updateTemporaryEntry(value);
+		}, 500);
 	};
 }()
+
+// all functions accept jQuery objects as elements and unix time offsets as times
+var Entry = {
+	create: function(time, message) {
+		return $("#items").append(
+			"<li class=\"entry\">" +
+				"<div class=\"time\" data-time=\"" + time + "\"></div>" +
+				"<div class=\"message\">" + message + "</div>" +
+			"</li>");
+	},
+	remove: function(element) {
+		element.remove();
+	},
+	time: function(element, newTime) {
+		var value = element.find(".time");
+		if (newTime === null) {
+			value.removeData("time");
+		}
+		else if (!newTime) {
+			var timestamp = value.data("time");
+			
+			if (typeof timestamp === "undefined")
+				return null;
+			return timestamp;
+		}
+		else
+			value.data("time", newTime);
+		Entry.updateTime(element);
+	},
+	message: function(element, newMessage) {
+		var value = element.find(".message");
+		if (!newMessage)
+			return value.html();
+		value.html(newMessage);
+	},
+	updateTime: function(element) {
+		var timestamp = Entry.time(element);
+		if (timestamp === null) {
+			element.find(".time").text("");
+			element.removeClass("past");
+		}
+		else {
+			var time = moment.unix(timestamp);
+			element.find(".time").text(time.fromNow());
+			if (time.isBefore())
+				element.addClass("past");
+			else
+				element.removeClass("past");
+		}
+	}
+}
